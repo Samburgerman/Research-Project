95dd56ccd15a95283f98f39bae164b6e882af884
Samburgerman <104079001+Samburgerman@users.noreply.github.com>
2024-01-19 14:38:14 -0600

    Grounded test works properly now

--- Assets/Scripts/GroundSensor.cs
+++ Assets/Scripts/GroundSensor.cs
@@ -1,7 +1,20 @@
-using System;using System.Collections;using UnityEditor.Timeline.Actions;using UnityEngine;public class GroundSensor : MonoBehaviour{    [SerializeField] private string groundTag = "Ground";    [SerializeField] private int numOnDice = -1;    [SerializeField] private Rigidbody rb;    [SerializeField] private Dice dice;
+using System;using System.Collections;using UnityEditor.Timeline.Actions;using UnityEngine;public class GroundSensor : MonoBehaviour{    [SerializeField] private string groundTag = "Ground";    [SerializeField] private int numOnDice = -1;    [SerializeField] private Rigidbody rb;    [SerializeField] private float waitSeconds = 0.1f;    [SerializeField] private Dice dice;
 
-    //discussion unity com how do i return a value from a corouitne
+    private bool isGrounded = false;
 
-    private void OnTriggerEnter(Collider other)    {        CoroutineWithData groundedTest = new CoroutineWithData(this,Grounded());        //bool isGrounded = (bool)(groundedTest.result);
-        print("CoroutineWithData.result: "+groundedTest.result/*+" isGrounded: "+isGrounded*/);    }    private IEnumerator Wait()    {        yield return new WaitForSeconds(2.0f);    }    private IEnumerator Grounded()    {        yield return Wait();        if(rb.velocity.magnitude<0.1f)            yield return true;        else
-            yield return false;    }}public class CoroutineWithData{    public Coroutine coroutine { get; private set; }    public object result;    private IEnumerator target;    public CoroutineWithData(MonoBehaviour owner,IEnumerator target)    {        this.target=target;        this.coroutine=owner.StartCoroutine(Run());    }    private IEnumerator Run()    {        while(target.MoveNext())        {            result=target.Current;            yield return result;        }    }}
+    private void OnTriggerEnter(Collider other)    {        StartCoroutine(Grounded());        print("collision detected");    }    private void UpdateIsGrounded(bool isGrounded)
+    {
+        this.isGrounded = isGrounded;
+        print(this.isGrounded);
+    }    private IEnumerator Wait(float seconds)    {        yield return new WaitForSeconds(seconds);    }    private IEnumerator Grounded()    {        print("begin");        yield return Wait(waitSeconds);        GroundedData groundedData = new GroundedData(rb.velocity.magnitude<0.1f);        UpdateIsGrounded(groundedData.GetIsGrounded());        print("end");    }}public struct GroundedData
+{
+    private bool isGrounded;
+    public GroundedData(bool isGrounded)
+    {
+        this.isGrounded = isGrounded;
+    }
+    public bool GetIsGrounded() { return isGrounded; }
+
+    public override string ToString()
+    { return ""+GetIsGrounded(); }
+}
