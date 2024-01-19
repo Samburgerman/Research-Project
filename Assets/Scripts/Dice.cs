5b8e7f6d3270a2468d954aaa7f45b1149720c8bb
Samburgerman <104079001+Samburgerman@users.noreply.github.com>
2024-01-19 16:26:28 -0600

    Range scriptable object is now working

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,9 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class Dice : MonoBehaviour
 {
+    [SerializeField] private Range position;
+    [SerializeField] private Range eulerAngles;
+    [SerializeField] private Range velocity;
+    [SerializeField] private Range angularVelocity;
+
+
+    public int rollNumber { get; set; }
+
     public void Roll()
     {
         //apply some form of force and or torque idk
+    }
+}
+
+[CreateAssetMenu]
+public class Range : ScriptableObject
+{
+    public Vector3 lower;
+
+    public Vector3 upper;
+
+    public Range(Vector3 lower,Vector3 upper)
+    { this.lower = lower; this.upper = upper; }
+
+    public Vector3 Lerp(float amountThroughRange)
+    {
+        return new Vector3Lerp().Operation(lower,upper,amountThroughRange);
     }
 }
--- Assets/Scripts/GroundSensor.cs
+++ Assets/Scripts/GroundSensor.cs
@@ -1,11 +1,13 @@
-using System;using System.Collections;using UnityEditor.Timeline.Actions;using UnityEngine;public class GroundSensor : MonoBehaviour{    [SerializeField] private string groundTag = "Ground";    [SerializeField] private int numOnDice = -1;    [SerializeField] private Rigidbody rb;    [SerializeField] private float waitSeconds = 0.1f;    [SerializeField] private Dice dice;
+using System;using System.Collections;using UnityEditor.Timeline.Actions;using UnityEngine;public class GroundSensor : MonoBehaviour{    [SerializeField] private string groundTag = "Ground";    [SerializeField] private int faceOnDice = -1;    [SerializeField] private Rigidbody rb;    [SerializeField] private float waitSeconds = 0.1f;    [SerializeField] private Dice dice;
 
     private bool isGrounded = false;
+    private const float epsilon = 0.1f;
 
-    private void OnTriggerEnter(Collider other)    {        StartCoroutine(Grounded());    }    private void UpdateIsGrounded(bool isGrounded)
+    private void OnTriggerEnter(Collider other)    {        if(other.CompareTag(groundTag))            StartCoroutine(Grounded());    }    private void UpdateIsGrounded(bool isGrounded)
     {
         this.isGrounded = isGrounded;
-    }    private IEnumerator Wait(float seconds)    {        yield return new WaitForSeconds(seconds);    }    private IEnumerator Grounded()    {        yield return Wait(waitSeconds);        GroundedData groundedData = new GroundedData(rb.velocity.magnitude<0.1f);        UpdateIsGrounded(groundedData.GetIsGrounded());    }}public struct GroundedData
+        dice.rollNumber=faceOnDice;
+    }    private IEnumerator Wait(float seconds)    {        yield return new WaitForSeconds(seconds);    }    private IEnumerator Grounded()    {        yield return Wait(waitSeconds);        GroundedData groundedData = new GroundedData(rb.velocity.magnitude<epsilon            &&rb.angularVelocity.magnitude<epsilon);        UpdateIsGrounded(groundedData.GetIsGrounded());    }}public struct GroundedData
 {
     private bool isGrounded;
     public GroundedData(bool isGrounded)
