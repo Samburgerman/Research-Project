6d3eb74e006be005ea3bf5d6790b15d3e7b5d166
Samburgerman <104079001+Samburgerman@users.noreply.github.com>
2024-02-21 13:38:49 -0600

    Game looped called by recursive sequence.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,29 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("ComponentReferences")]
     [SerializeField] private BoardCreator boardCreator;
     [SerializeField] private DiceFaceLogic diceFaceLogic;
     [SerializeField] private PieceGenerator pieceGenerator;
     [SerializeField] private PieceMover pieceMover;
-    public static int TotalSpaces { get; private set; } = 12;
     [SerializeField] private float radius = 5;
 
-    private List<Piece> pieces = new();
-    private GameStates gameStates = new(new());
-
-    public int TurnNumber { private set; get; } = 0;
+    [Header("GameRules")]
     [SerializeField] private int totalTurnsInGame = 10;
     [SerializeField] private int startSpace = 0;
     [SerializeField] private int startMoney = 10;
 
-    public static int numStepsInSimulation = 500;
-
+    [Header("Time")]
     [SerializeField] private float timeScale = 1.0f;
     [SerializeField] private float waitBetweenTurns = 1.0f;
+    [SerializeField] private float waitBetweenRounds = 4.0f;
+
+
+    public int TurnNumber { private set; get; } = 0;
+    public static int TotalSpaces { get; private set; } = 12;
+    public static int numStepsInSimulation = 500;
     public static float waitAfterDiceDisplay = 1.5f;
+
+    private List<Piece> pieces = new();
+    private GameStates gameStates = new(new());
 
     public int GetPlayerPos(int playerIndex)
     { return pieces[playerIndex].GetPlayerData().spaceNumber; }
@@ -33,7 +39,7 @@ public class GameManager : MonoBehaviour
     {
         InitializeBoard();
         JsonLogger.LogJson(gameStates);
-        GameLoop();
+        GameRecursiveSequence();
         Time.timeScale=timeScale;
     }
 
@@ -43,20 +49,37 @@ public class GameManager : MonoBehaviour
         pieces=pieceGenerator.GeneratePieces(startSpace,startMoney);
     }
 
-    private void GameLoop()
+    private void GameRecursiveSequence()
     {
-        while(!IsGameOver())
+        if(!IsGameOver())
         {
             TurnNumber++;
             //each player takes a turn moving around the board
-            foreach(Piece piece in pieces)
-            {
-                PlayerTurn(piece);
-                StartCoroutine(Wait(waitBetweenTurns));
-            }
-            gameStates.Add(GetGameState());
+            StartCoroutine(RecursiveTurns(0));
         }
-        JsonLogger.LogJson(gameStates);
+        else
+            JsonLogger.LogJson(gameStates);
+    }
+
+    private IEnumerator RecursiveTurns(int i)
+    {
+        if(i>=pieces.Count)
+        {
+            gameStates.Add(GetGameState());
+            StartCoroutine(CallGameSequenceFunctionAfterWait());
+        }
+        else
+        {
+            yield return new WaitForSeconds(waitBetweenTurns);
+            PlayerTurn(pieces[i]);
+            StartCoroutine(RecursiveTurns(i+1));
+        }
+    }
+
+    private IEnumerator CallGameSequenceFunctionAfterWait()
+    {
+        yield return new WaitForSeconds(waitBetweenRounds);
+        GameRecursiveSequence();
     }
 
     private void PlayerTurn(Piece piece)
@@ -84,7 +107,7 @@ public static class JsonLogger
 {
     public static void LogJson(object o)
     {
-        string jsonOutput = JsonUtility.ToJson(o);
+        string jsonOutput = JsonUtility.ToJson(o,true);
         File.WriteAllText(Application.dataPath+"/JsonLogs/data.txt",jsonOutput);
     }
 }
